cc069b56622ab52468b18c12006e2f0153054b7b
Vo Trung Hau <khongcoten3546@gmail.com>
2026-07-19 13:28:48 +0700

    Cap nhat IsLocked va Summernote

--- Migrations/20260719053644_Lab17_AddUserIsLocked.Designer.cs
+++ Migrations/20260719053644_Lab17_AddUserIsLocked.Designer.cs
@@ -4,6 +4,7 @@ using AHUWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AHUWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260719053644_Lab17_AddUserIsLocked")]
+    partial class Lab17_AddUserIsLocked
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
